f8fdbafb017dd13bc5daeffa8ce94e8bdc1c9c4f
raynamharris <rayna.harris@utexas.edu>
2016-03-25 22:03:28 -0500

    added bootstrap and updated modules

--- 05_kallistoquant_cmds.sh
+++ 05_kallistoquant_cmds.sh
@@ -23,7 +23,7 @@ for R1 in *R1_001.trimmed_filtered.fastq.gz; do
     samp=$(basename $R1 _R1_001.trimmed_filtered.fastq.gz)
     echo $R1 $R2 $samp
     cat >> 05_kallistoquant_cmds.cmds <<EOF
-kallisto quant -i /work/02189/rmharris/SingleNeuronSeq/data/reference_genomes/combo.idx -b 100 -o /scratch/02189/rmharris/SingleNeuronSeq/results/05_kallistoquant_2016-03-16/${samp} $R1 $R2
+kallisto quant -b 100 -i /work/02189/rmharris/SingleNeuronSeq/data/reference_genomes/combo.idx  -o /scratch/02189/rmharris/SingleNeuronSeq/results/05_kallistoquant_2016-03-24/${samp} $R1 $R2
 EOF
 done
 
