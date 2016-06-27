7400f37bbe0017584cacd52cb1688b23d897ade5
raynamharris <rayna.harris@utexas.edu>
2016-06-27 19:24:58 -0400

    kallisto update 32 STG

--- 05_kallistoquant_cmds.sh
+++ 05_kallistoquant_cmds.sh
@@ -1,60 +1,27 @@
 #!/bin/bash
 
-## this script creates a list of top hat commands to execute fore every file in the folder
-## the outhput is a .cmds file that will be called in the 05_kallistoquant_launcher.slurm
+## this script creates a kallistoquant command for every file in the working direcotry
+## the outhput is a .cmds file that is referenced in 05_kallistoquant_launcher.slurm
 
 ## make output directory on scratch
-#mkdir /scratch/02189/rmharris/SingleNeuronSeq/results/05_kallistoquant_ERCC92_2016-03-25
-#mkdir /scratch/02189/rmharris/SingleNeuronSeq/results/05_kallistoquant_MusUCSD_2016-03-25
-
-mkdir /scratch/02189/rmharris/SingleNeuronSeq/JA16268/2016-06-03-05-Cborealis-cds-kallisto
-#mkdir /scratch/02189/rmharris/SingleNeuronSeq/JA16268/2016-06-02-05-Cborealis-transcriptome-kallisto
+mkdir /scratch/02189/rmharris/SingleNeuronSeq/results/JA16393/2016-06-27-kallistoquant
 
 
-## the $WORKING_DIRECTORY
-cd /scratch/02189/rmharris/SingleNeuronSeq/JA16268/2016-05-24-trimmedreads
+## the $WORKDIR
+cd /work/02189/rmharris/IntMolModule/STG/data/2016-06-27-JA16393
 
 
-## in case there is an older version, let's delete the file, so we don't append to it
-#rm 05_kallistoquant_ERCC92.cmds
-#rm 05_kallistoquant_MusUCSD.cmds
-rm 05_kallistoquant_Cborealis_cds.cmds
-rm 05_kallistoquant_Cborealis_transcriptome.cmds
+## now, create 2016-06-27-kallistoquant.cmds 
 
-
-## now, create 05_kallistoquant_Cborealis.cmds with all a kallisto quant command for each sample
-
-## this for loop will append a kallistoquant command for sample to 05_kallistoquant_Cborealis_cds.cmds
-
-for R1 in PD*R1_001.trimmed.fastq GM*R1_001.trimmed.fastq; do
-    R2=$(basename $R1 R1_001.trimmed.fastq)R2_001.trimmed.fastq
-    samp=$(basename $R1 _R1_001.trimmed.fastq)
+for R1 in *R1_001.fastq.gz; do
+    R2=$(basename $R1 R1_001.fastq.gz)R2_001.fastq.gz
+    samp=$(basename $R1 _R1_001.fastq.gz)
     echo $R1 $R2 $samp
-    cat >> 05_kallistoquant_Cborealis_cds.cmds <<EOF
-kallisto quant -b 100 -i /scratch/02189/rmharris/Cborealis_ref/Cborealis_candidategenes.idx  -o /scratch/02189/rmharris/SingleNeuronSeq/JA16268/2016-06-03-05-Cborealis-cds-kallisto/${samp} $R1 $R2
+    cat >> 2016-06-27-kallistoquant.cmds <<EOF
+kallisto quant -b 100 -i /scratch/02189/rmharris/Cborealis_ref/Cborealis_transcriptome.idx  -o /scratch/02189/rmharris/SingleNeuronSeq/results/JA16393/2016-06-27-kallistoquant/${samp} $R1 $R2
 EOF
 done
 
-## this for loop will append a kallisto quant command for sample to 05_kallistoquant_Cborealis_transcriptome.cmds
-for R1 in PD*R1_001.trimmed.fastq GM*R1_001.trimmed.fastq; do
-    R2=$(basename $R1 R1_001.trimmed.fastq)R2_001.trimmed.fastq
-    samp=$(basename $R1 _R1_001.trimmed.fastq)
-    echo $R1 $R2 $samp
-    cat >> 05_kallistoquant_Cborealis_transcriptome.cmds <<EOF
-kallisto quant -b 100 -i /scratch/02189/rmharris/Cborealis_ref/Cborealis_transcriptome.idx  -o /scratch/02189/rmharris/SingleNeuronSeq/JA16268/2016-06-02-05-Cborealis-transcriptome-kallisto/${samp} $R1 $R2
-EOF
-done
 
 ## check number of lines in commands file
-wc 05_kallistoquant_Cborealis_cds.cmds
-wc 05_kallistoquant_Cborealis_transcriptome.cmds 
-
-## now this makes a new file. to execute the follow command from bin
-## edit the 05_kallistoquant_launcher.slurm script  then launch
-# sbatch 05_kallistoquant_launcher.slurm  
-
-
-
-
-
-
+wc 2016-06-27-kallistoquant.cmds
